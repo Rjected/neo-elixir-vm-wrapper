409b42c2bcac8dd19b4203494df327db563122f3
Rjected <kidscline@gmail.com>
2018-03-17 11:19:59 -0400

    Created encoders and decoders for stackitem

--- neo_elixir_vim_wrapper/lib/neo_elixir_vim_wrapper.ex
+++ neo_elixir_vim_wrapper/lib/neo_elixir_vim_wrapper.ex
@@ -1,9 +1,13 @@
 defmodule NeoElixirVimWrapper do
-    @python_dir Application.app_dir(:neo_elixir_vim_wrapper, "priv/python")
+    @python_dir Path.expand("~/.neo-elixir-vm-wrapper/neo-python/neo/VM") 
     use Export.Python
-
-    def test( str ) do 
+    
+    @spec isArray(tuple()) :: boolean()
+    def isArray( tuple ) do
         {:ok, py} = Python.start(python_path: @python_dir)
-        py |> Python.call(test( str ), from_file: "pythonThing")
+         
+        py |> Python.call(stackitem_decoder(elem(tuple, 1)), from_file: "InteropService") 
     end
+    
+
 end
